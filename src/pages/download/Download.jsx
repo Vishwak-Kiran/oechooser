9ed39446711cca4455461f1358af3cc4f2a0288d
gokul <gokul465eswaran@gmail.com>
2023-05-11 21:53:21 +0530

    table and react print for

--- src/pages/download/Download.jsx
+++ src/pages/download/Download.jsx
@@ -1,74 +1,56 @@
 import React, { useRef, useState } from "react";
-
+import ReactToPrint from "react-to-print";
 import { useCollection } from "../../hooks/useCollection";
 import ProjectList from "../../components/ProjectList";
 import ProjectFilter from "../dashboard/ProjectFilter";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import "./Download.css";
 
 export default function Download() {
   const { user } = useAuthContext();
   const [field, setfield] = useState(false);
-  const { documents, error } = useCollection("Water Management");
+  const { documents, error } = useCollection("Web Design and Management");
   const [OE, setOE] = useState("Machine Learning");
   const [filter, setFilter] = useState({});
   const componentRef = useRef();
 
   const changeFilter = () => {
     documents.filter((document) => {
-      //  console.log(document.students[1]);
       setfield(true);
-      // document.students((u) => {
-      //   // if (STUDENTS === "IT") {
-      //   //   return u;
-      //   // }
-      //   console.log(u);
-      // });
     });
   };
-  return (
-    <div
-      style={{
-        width: "100vw",
-        alignItems: "center",
-        justifyItems: "center",
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-      }}
-    >
-      {console.log("hello")} <div ref={componentRef}></div>
-      <h1 style={{ fontSize: "10rem" }}>Gokul</h1>
-      <button
-        style={{ width: "10rem", height: "10rem" }}
-        className="btn"
-        onClick={changeFilter}
-      >
-        click to print
-      </button>
-      <h1>the students in {OE} </h1>
-      {
-        field &&
-          documents &&
-          documents.map((document) => (
-            <ul>
-              <p>{document.students[1]}</p>
-              <p>{document.students[2]}</p>
-            </ul>
-          ))
-        // <h1>the students in {documents[0].students[1]}</h1>
-      }
-      {/* {console.log(field, documents)}
-      {field &&
-        documents.map((document) => {
-          {
-            console.log(document.students[1]);
-          }
-          <div>
-            {console.log("works 2x", document.students[1])}
 
-            <ul>{document.students[1]}</ul>
-          </div>;
-        })} */}
+  return (
+    <div className="download-container">
+      <h1 className="download-header">the students in {OE} </h1>
+      <button className="btn download-btn" onClick={changeFilter}>
+        Show all enrolled
+      </button>
+      {field && documents && (
+        <table className="download-table" ref={componentRef}>
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Student Name</th>
+            </tr>
+          </thead>
+
+          {documents.map((document) => (
+            <tbody>
+              <tr key={document.id}>
+                <td>{document.students[1]}</td>
+                <td>{document.students[2]}</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      )}
+      <ReactToPrint
+        trigger={() => (
+          <button className="btn download-btn">Print this out!</button>
+        )}
+        content={() => componentRef.current}
+      />
     </div>
   );
 }

c111c4baff104697c9568f97c2965a281f1debb5
gokul <gokul465eswaran@gmail.com>
2023-05-14 22:39:26 +0530

    changes

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -117,6 +117,8 @@ export default function Signup() {
           <option value="EIE">
             Electronics and Instrumentation Engineering
           </option>
+          <option value="BME">Biomedical Engineering</option>
+          <option value="RIE"> Robotics and Automation Engineering</option>
 
           <option value="CE">Civil Engineering</option>
           <option value="MECH">Mechanical Engineering</option>

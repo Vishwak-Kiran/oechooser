f7664ebbd834c4c867102efe63e711364f5a1be1
gokul <gokul465eswaran@gmail.com>
2023-09-19 12:24:02 +0530

    updated excel csv

--- src/pages/download/Download.jsx
+++ src/pages/download/Download.jsx
@@ -1,63 +1,80 @@
-import React, { useRef, useState } from "react";
-import ReactToPrint from "react-to-print";
+import React, { useState } from "react";
 import { useCollection } from "../../hooks/useCollection";
-import ProjectList from "../../components/ProjectList";
-import ProjectFilter from "../dashboard/ProjectFilter";
 import { useAuthContext } from "../../hooks/useAuthContext";
-import "./Download.css";
+import { saveAs } from "file-saver";
+import ExcelJS from "exceljs";
+import "./Download.css"; // Import your CSS file
 
 export default function Download() {
   const { user } = useAuthContext();
-  const [field, setfield] = useState(false);
-  const { documents, error } = useCollection("Web Design and Management");
-  const [OE, setOE] = useState("Machine Learning");
-  const [filter, setFilter] = useState({});
-  const componentRef = useRef();
+  const [selectedCollection, setSelectedCollection] = useState(
+    "Web Design and Management"
+  );
 
-  const changeFilter = () => {
-    documents.filter((document) => {
-      setfield(true);
-    });
+  // Define the available collection options
+  const collectionOptions = [
+    "Pudhu Computing",
+    "Web Design and Management",
+    "Cloud Computing",
+    "Machine Learning",
+  ];
+
+  const { documents, error } = useCollection(selectedCollection);
+
+  const downloadAsExcel = async () => {
+    try {
+      // Create a new workbook
+      const workbook = new ExcelJS.Workbook();
+      const worksheet = workbook.addWorksheet("Students");
+
+      // Define the columns in the worksheet
+      worksheet.columns = [
+        { header: "Email", key: "email" },
+        { header: "Student Name", key: "studentName" },
+        { header: "Enrolled Time", key: "enrolledTime" },
+        { header: "Elective", key: "elective" },
+      ];
+
+      // Populate the worksheet with data
+      documents.forEach((document) => {
+        worksheet.addRow({
+          email: document.students[1],
+          studentName: document.students[2],
+          enrolledTime: new Date(document.createdAt.toDate()).toLocaleString(),
+          elective: document.name,
+        });
+      });
+
+      // Create a blob from the workbook
+      const blob = await workbook.xlsx.writeBuffer();
+
+      // Create a Blob object and trigger download
+      saveAs(new Blob([blob]), "students.xlsx");
+    } catch (error) {
+      console.error("Error exporting data:", error);
+    }
   };
 
   return (
-    <div style={{zIndex:"0"}} className="designCard download-container">
-      <h1 className="download-header">The Students in {OE} </h1>
-      <button className="btn download-btn" onClick={changeFilter}>
-        Show all enrolled
-      </button>
-      {field && documents && (
-        <table className="download-table" ref={componentRef}>
-          <thead>
-            <tr>
-              <th>Email</th>
-              <th>Student Name</th>
-              <th>Enrolled Time</th>
-              <th>Elective</th>
-            </tr>
-          </thead>
-
-          {documents.map((document) => (
-            <tbody>
-              <tr key={document.id}>
-                {console.log(document.createdAt)}
-                <td>{document.students[1]}</td>
-                <td>{document.students[2]}</td>
-                <td>
-                  {new Date(document.createdAt.toDate()).toLocaleString()}
-                </td>
-                <td>{document.name}</td>
-              </tr>
-            </tbody>
+    <div className="download-container">
+      <h1 className="download-header">Students Enrolled</h1>
+      <div className="select-container">
+        <label htmlFor="collectionSelect">Select Collection:</label>
+        <select
+          id="collectionSelect"
+          value={selectedCollection}
+          onChange={(e) => setSelectedCollection(e.target.value)}
+        >
+          {collectionOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
           ))}
-        </table>
-      )}
-      <ReactToPrint
-        trigger={() => (
-          <button className="btn download-btn">Print this out!</button>
-        )}
-        content={() => componentRef.current}
-      />
+        </select>
+      </div>
+      <button className="btn download-btn" onClick={downloadAsExcel}>
+        Download as Excel
+      </button>
     </div>
   );
 }
